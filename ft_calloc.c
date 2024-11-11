2d5c8b93a68c74136420aa66ed77f7355861945c
Abderrahman Gaga <agaga@c2r6p13.hive.fi>
2024-11-11 17:54:13 +0200

    new file:   Makefile 	new file:   ft_atoi.c 	new file:   ft_bzero.c 	new file:   ft_calloc.c 	new file:   ft_isalnum.c 	new file:   ft_isalpha.c 	new file:   ft_isascii.c 	new file:   ft_isdigit.c 	new file:   ft_isprint.c 	new file:   ft_itoa.c 	new file:   ft_lstadd_back_bonus.c 	new file:   ft_lstadd_front_bonus.c 	new file:   ft_lstclear_bonus.c 	new file:   ft_lstdelone_bonus.c 	new file:   ft_lstiter_bonus.c 	new file:   ft_lstlast_bonus.c 	new file:   ft_lstmap_bonus.c 	new file:   ft_lstnew_bonus.c 	new file:   ft_lstsize_bonus.c 	new file:   ft_memchr.c 	new file:   ft_memcmp.c 	new file:   ft_memcpy.c 	new file:   ft_memmove.c 	new file:   ft_memset.c 	new file:   ft_putchar_fd.c 	new file:   ft_putendl_fd.c 	new file:   ft_putnbr_fd.c 	new file:   ft_putstr_fd.c 	new file:   ft_split.c 	new file:   ft_strchr.c 	new file:   ft_strdup.c 	new file:   ft_striteri.c 	new file:   ft_strjoin.c 	new file:   ft_strlcat.c 	new file:   ft_strlcpy.c 	new file:   ft_strlen.c 	new file:   ft_strmapi.c 	new file:   ft_strncmp.c 	new file:   ft_strnstr.c 	new file:   ft_strrchr.c 	new file:   ft_strtrim.c 	new file:   ft_substr.c 	new file:   ft_tolower.c 	new file:   ft_toupper.c 	new file:   libft.h

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaga <agaga@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 13:31:16 by agaga             #+#    #+#             */
+/*   Updated: 2024/11/11 17:05:57 by agaga            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_memset(ptr, 0, count * size);
+	return (ptr);
+}
